fa6e51d812483f8a2372f6c66fe60020a5f3b164
GuilhermeFGL <guilherme_fgl@hotmail.com>
2019-10-07 09:22:01 -0300

    add tsconfig

---
+++ dist/vars.js
@@ -0,0 +1,16 @@
+var isCreated = true;
+var isActive;
+isActive = false;
+var index = 1;
+var count;
+count = 0;
+var title = "title";
+var text;
+text = "text";
+var object; // accept any value
+object = false;
+object = "object";
+// arrays
+var a1 = [1, 2, 3];
+var a2 = [1, 2, 3];
+var a3 = [1, 2, 3];
